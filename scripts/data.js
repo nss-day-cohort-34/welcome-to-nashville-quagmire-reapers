740f3e16898afb589b124f1bce2b2cc6d2b182d9
jasonwheeler <jasonawheeler615@yahoo.com>
2019-07-31 09:03:43 -0500

    updated search buttons work code cleaned up

--- scripts/data.js
+++ scripts/data.js
@@ -1,39 +1,3 @@
-// const concertData = () => {
-//     return fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=AoIwW2jsbdQE4QLPUEqCsZWuic9ZCu1X&classificationName=music&dmaId=343")
-//         .then(response => response.json())
-//         // .then(data => {
-//         //     console.log(data)
-//         //     data.message
-//         // })
-//  }
-
-
-
-// concertData()
-
-// const ticketmasterFetch = (search) => {
-//     fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&size=50&apikey=AoIwW2jsbdQE4QLPUEqCsZWuic9ZCu1X")
-//         .then(response => response.json())
-//         .then(responses => {
-//             console.log(responses)
-//             // console.log(responses._embedded.events[0].classifications[0].genre.name)
-//             for (let i = 0; i < responses._embedded.events.length; i++) {
-//                 let genreName = responses._embedded.events[i].classifications[0].genre.name
-//                 let eventName = responses._embedded.events[i].name
-//                 if (search === genreName) {
-//                     return eventName
-//                     const tmHtml = ticketmasterHTML(eventName)
-//                     renderToDOM(tmHtml)
-//                 }
- 
-//             }
- 
-//         })
- 
-//  }
-
-//  ticketmasterFetch()
-
 
 // BEGIN PARKS SECTION
 
--- scripts/dom.js
+++ scripts/dom.js
@@ -1,19 +1,4 @@
 
-// renderConcertsResultsHTML.innerHTML += concertsHTMLResultsRep()
-
-
-
-
-// const searchButton = document.querySelector("#concertSearchButton")
-// searchButton.addEventListener("click", () => {
-//     for (let i = 0; i < responses._embedded.events.length; i++) {
-//         let genreName = responses._embedded.events[i].classifications[0].genre.name
-//         let eventName = responses._embedded.events[i].name
-//         if (search === genreName) {
-//             return eventName
-//             const tmHtml = ticketmasterHTML(eventName)
-//             renderToDOM(tmHtml)
-// }
 
 
 // BEGIN PARKS SECTION
